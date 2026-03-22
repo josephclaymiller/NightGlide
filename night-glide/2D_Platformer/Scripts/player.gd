d3d89ab7531e66238fbcfc9ce13e8bf097882469
Joseph Miller <josephclaymiller@users.noreply.github.com>
2026-03-22 10:17:10 -0700

    climbing

--- night-glide/2D_Platformer/Scripts/player.gd
+++ night-glide/2D_Platformer/Scripts/player.gd
@@ -4,32 +4,38 @@ extends CharacterBody2D
 const SPEED: float = 300.0
 const JUMP_VELOCITY: float = -500.0
 const GRAVITY_FALL: float = 200.0
+const MASK_WORLD: int = 1        # layer 1
+const MASK_CLIMBABLE: int = 4    # layer 3 (bit 2 = value 4)
 
 var is_dead: bool = false
 var is_gliding: bool = false
+var on_tree: bool = false
+var is_climbing: bool = false
 
 func _physics_process(delta: float) -> void:
 	if is_dead:
 		return
-		
-	# Glide while not on ground
+	
+	if is_climbing:
+		_handle_climb()
+	else:
+		# Re-enable collision with climbable layer when not climbing
+		#set_collision_mask_value(3, true)
+		_handle_fall(delta)
+	
+	_handle_jumps()	
+	
+	_handle_movement()
+	move_and_slide()
+	_check_collisions()
+	_check_if_dead()
+	
+func _handle_fall(delta: float) -> void:
 	if not is_on_floor():
 		if is_gliding:
 			_glide(delta)
 		else:
 			_fall(delta)
-			
-	# Check if jump key pressed an handle jump
-	_handle_jumps()
-	
-	# Check if left/right key pressed and handle movement
-	_handle_movement()
-	
-	# Check if dead and end game
-	_check_enemy_collision()
-	
-	# Move player according to left/right movement and jump/glide/fall
-	move_and_slide()
 	
 func _handle_jumps() -> void:
 	# Check if jump key pressed
@@ -63,15 +69,33 @@ func _handle_movement() -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 		
-func _check_enemy_collision() -> void:
+func _handle_climb() -> void:
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = SPEED
+	else:
+		velocity.y = 0.0
+		
+func _check_collisions() -> void:
+	is_climbing = false
 	for i: int in get_slide_collision_count():
 		var collision: KinematicCollision2D = get_slide_collision(i)
 		var collider: Object = collision.get_collider()
 		if collider is CharacterBody2D and collider.is_in_group("enemies"):
-			_die()
+			is_dead = true
 			return
+		if collider.is_in_group("tree"):
+			is_climbing = true
 
-func _die() -> void:
-	is_dead = true
+func _check_if_dead() -> void:
+	if is_dead:
+		_end_game()
+	# check if fell of world
+	if position.y > 680:
+		_end_game()
+
+func _end_game() -> void:
 	# Tell World to show the game over screen
+	self.visible = false
 	get_tree().get_root().get_node("World_2D_Platformer/GameOverScreen").visible = true
---
+++ night-glide/2D_Platformer/Scripts/tree.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		print("Player exited tree")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		print("Player touched tree")

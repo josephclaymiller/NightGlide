1a4ed6d330f7288f9b19ecd9e7eadcc8faa9c7de
Joseph Miller <josephclaymiller@users.noreply.github.com>
2026-03-23 21:59:42 -0700

    climbing

--- night-glide/3D_Platformer/Scripts/player.gd
+++ night-glide/3D_Platformer/Scripts/player.gd
@@ -2,7 +2,11 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const ROTATION_SPEED: float = 3.0
 const JUMP_VELOCITY = 4.5
+
+
+var is_climbing: bool = false
 
 
 func _physics_process(delta: float) -> void:
@@ -14,15 +18,43 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	if is_climbing:
+		_handle_climb()
+	
+	_handle_movement(delta)
+		
+	_check_collisions()
+
+	move_and_slide()
+
+func _handle_climb() -> void:
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = -SPEED
+	else:
+		velocity.y = 0.0
+
+func _handle_movement(delta: float) -> void:
+		# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Rotate left/right
+	rotation.y -= input_dir.x * ROTATION_SPEED * delta
+	
 	if direction:
 		velocity.x = -direction.x * SPEED
 		velocity.z = -direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		
+func _check_collisions() -> void:
+	is_climbing = false
+	for i: int in get_slide_collision_count():
+		var collision: KinematicCollision3D = get_slide_collision(i)
+		var collider: Object = collision.get_collider()
+		if collider.is_in_group("tree"):
+			is_climbing = true
